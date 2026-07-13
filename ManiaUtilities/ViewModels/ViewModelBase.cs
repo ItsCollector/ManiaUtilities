e7a1e36931926b728f58d60121f7914864eb34ca
ItsCollector <85847968+ItsCollector@users.noreply.github.com>
2026-07-13 16:00:27 +0100

    init

---
+++ ManiaUtilities/ViewModels/ViewModelBase.cs
@@ -0,0 +1,7 @@
+﻿using CommunityToolkit.Mvvm.ComponentModel;
+
+namespace ManiaUtilities.ViewModels;
+
+public abstract class ViewModelBase : ObservableObject
+{
+}
---
+++ ManiaUtilities/Views/MainWindow.axaml.cs
@@ -0,0 +1,11 @@
+using Avalonia.Controls;
+
+namespace ManiaUtilities.Views;
+
+public partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
