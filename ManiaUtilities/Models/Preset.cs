079e4f89ec14ad72ce6bf6187088ff1c70457347
ItsCollector <85847968+ItsCollector@users.noreply.github.com>
2026-07-15 01:41:55 +0100

    Created UI for map information and presets
    These aren't hooked up to anything yet, still learning how to use Avalonia mainly.
    
    I am definitely going to change the presets to a customisable dropdown in the future and position it underneath the map infomation card.  This will be next to a button that adds modules into view.

---
+++ ManiaUtilities/Models/Preset.cs
@@ -0,0 +1,9 @@
+﻿namespace ManiaUtilities.Models;
+
+public class Preset
+{
+    public string Name { get; set; } = string.Empty;
+    public bool FlnConversionEnabled  { get; set; }
+    public int FlnGapMs { get; set; }
+    public bool RemoveLnsEnabled { get; set; }
+}
--- ManiaUtilities/ViewModels/MainViewModel.cs
+++ ManiaUtilities/ViewModels/MainViewModel.cs
@@ -1,8 +1,29 @@
 using CommunityToolkit.Mvvm.ComponentModel;
+using System.Collections.ObjectModel;
+using ManiaUtilities.Models;
 
 namespace ManiaUtilities.ViewModels;
 
 public partial class MainViewModel : ViewModelBase
 {
-    [ObservableProperty] private string _greeting = "Welcome to Avalonia!";
+    [ObservableProperty]
+    private ObservableCollection<Preset> presets = new()
+    {
+        new Preset { Name = "Convert to FLN", FlnConversionEnabled = true, FlnGapMs = 80 },
+        new Preset { Name = "Remove LNs", RemoveLnsEnabled = true }
+    };
+    
+    [ObservableProperty] private string mapTitle = "Placeholder Title";
+    [ObservableProperty] private string mapAuthor = "Placeholder Author";
+    [ObservableProperty] private string mapVersion = "Placeholder Version";
+    
+    [ObservableProperty]
+    private Preset? selectedPreset;
+
+    partial void OnSelectedPresetChanged(Preset? value)
+    {
+        if (value is null) return;
+        
+        selectedPreset = value;
+    }
 }
